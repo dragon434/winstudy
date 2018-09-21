8fc6af905d3a9e6f831da3d9ffddc0434f139d35
jiawenlong <jiawenlong@dingtalk.com>
2018-09-21 16:30:12 +0800

    hello

--- hello.py
+++ hello.py
@@ -14,8 +14,8 @@ print("-" * 50)
 while i < 11:
     n = 1
     while n <= 10:
-        # print("{:4d}".format(i * n), end=" ")
-        print("%s * %s = %s " %  i, n, (i * n)
+        print("{:4d}".format(i * n), end=" ")
+        # print("%s * %s = %s " %  i, n, (i * n)
         n += 1
     print()
     i += 1
